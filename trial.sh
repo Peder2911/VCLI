6f9656e714496b544ef061bc14ae11cfb56ed7b9
peder2911 <pglandsverk@gmail.com>
2020-07-29 16:36:58 +0200

    Working example
    Now is able to reproduce the quickstart from the documentation

---
+++ trial.sh
@@ -0,0 +1,8 @@
+
+./views estimate\
+   --dataset="cm_global_imp_0"\
+   --name="testmodel"\
+   --spec="os_acled_violence"\
+   --periodization="d_2020_04_01"\
+   --steps=1,2,3,4,5\
+   --est_args {n_estimators:100}
