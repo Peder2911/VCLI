a8adb14f9de6da57fbc65d7edcdf2acbd89997b8
peder2911 <pglandsverk@gmail.com>
2020-08-11 16:20:44 +0200

    Updated to v0.0.3
    Added specification / model inspect

--- vcli/explorer.py
+++ vcli/explorer.py
@@ -4,7 +4,7 @@ import os
 import logging
 import json
 
-from views.storage.orm import Variable,Varset,Theme,Model,RunPeriodization
+from views.storage.orm import Variable,Varset,Theme,Specification,RunPeriodization,Model
 from sqlalchemy.orm import sessionmaker
 
 import functools
@@ -79,11 +79,24 @@ class Explorer:
                 "\n".join(["\t"+str(tp) for tp in instance.periodsets])
             )
 
-    class models(OrmExplorer):
-        _model = Model
+    class specifications(OrmExplorer):
+        _model = Specification 
         def _inspect(self,instance):
             return compose(
-                    f"Model {instance.name}",
+                    f"Specification {instance.name}",
                     f"\t Composed of {len(instance.themes)} themes:",
                     "\n".join(["\t\t"+th.name for th in instance.themes])
+                )
+
+    class models(OrmExplorer):
+        _model = Model
+
+        def inspect(self,id):
+            i = self._getSession().query(self._model).filter(self._model.id == id).first()
+            return self._inspect(i)
+
+        def _inspect(self,instance):
+            return compose(
+                    f"Model of {instance.specification.name} with {instance.estimator.name}",
+                    f"\t{len(instance.runs)} runs"
                 )
